5ce97d5cc68c93284a89c1e94443d06fd2a30fc6
Michael Schmidt <m@ts.to>
2017-04-07 16:05:58 -0700

    fixes and enhancements

--- default-file.js
+++ default-file.js
@@ -8,32 +8,38 @@ var dir = "file"
 function DefaultFile(req, res){
     var DIR=process.cwd()
     Log("DefaultFile:",req.path,".")
-    var xpath = req.path.split("/").map(sanitize).join(path.sep)
-    var p = path.join(DIR, "file", xpath)
+    var tmp = req.path.split("/").map(sanitize)
+    var p = path.join(DIR, "file", req.path.split("/").map(sanitize).join(path.sep))
     if (fs.existsSync(p)) {
-	res.sendFile(p)
+        res.sendFile(p, function(err) {
+            if (err) res.status(404).send("<H1>404 File not Found</H1>")
+        })
     } else {
-	var sym = path.join(DIR,"file",req.path+".symlink")
-	if (fs.existsSync(sym)) {
-	    var file = fs.readFileSync(sym, "utf8")
-	    if (file[0] != "/") {
-		file = path.join(DIR, path.sep + dir + path.sep , file)
-	    }
-	    if (fs.existsSync(file)) {
-		res.sendFile(file)
-	    } else {
-		res.send("404 File not Found "+file)
-	    }
-	} else {
-	    res.send("404 File not Found")
-	}
+        var sym = path.join(DIR,"file",req.path+".symlink")
+        if (fs.existsSync(sym)) {
+            var file = fs.readFileSync(sym, "utf8")
+            if (file[0] != "/") {
+                file = path.join(DIR, path.sep + dir + path.sep , file)
+            }
+            if (fs.existsSync(file)) {
+                res.sendFile(file, function(err) {
+                    if (err) res.status(404).send("<H1>404 File not Found</H1>")
+                })
+            } else {
+                res.status(404).send("<H1>404 File not Found</H1>")
+            }
+        } else {
+            res.status(404).send("<H1>404 File not Found</H1>")
+        }
     }
 }
 
 module.exports = function(app,express,options) {
     if (options && typeof options == "string") {
-	dir = options.split("/").join(path.sep)
+        dir = options.split("/").join(path.sep)
     }
     Log("service DefaultFile")
     app.use(DefaultFile)
 }
+
+
